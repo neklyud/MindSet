4804d7ef68db5828dc212149ed0e137e538787a0
neklyud <xneklyudx@yandex.ru>
2020-03-30 23:11:47 +0300

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,18 @@
+import joblib
+import numpy as np
+from sklearn import datasets
+iris_X, iris_y = datasets.load_iris(return_X_y=True)
+np.unique(iris_y)
+# A random permutation, to split the data randomly
+np.random.seed(0)
+indices = np.random.permutation(len(iris_X))
+iris_X_train = iris_X[indices[:-10]]
+iris_y_train = iris_y[indices[:-10]]
+iris_X_test = iris_X[indices[-10:]]
+iris_y_test = iris_y[indices[-10:]]
+# Create and fit a nearest-neighbor classifier
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(iris_X_train, iris_y_train)
+
+joblib.dump(knn, 'knn.pkl')
